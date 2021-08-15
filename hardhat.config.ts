5171236d41a361c2a934b4765f58b70792fd921c
jayphbee <jayphhee@163.com>
2021-08-15 23:21:58 +0800

    upgrade oz

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -78,16 +78,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.6.12",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          }
-        }
-      },
-      {
-        version: "0.6.6",
+        version: "0.8.0",
         settings: {
           optimizer: {
             enabled: true,
