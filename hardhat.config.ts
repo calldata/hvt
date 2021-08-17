ef507fa87cd27b85f931a2b5a8b906060a8a741c
jayphbee <jayphbee@163.com>
2021-08-17 11:46:22 +0800

    upgrade solidity to 0.8.3

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -78,7 +78,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.0",
+        version: "0.8.3",
         settings: {
           optimizer: {
             enabled: true,
